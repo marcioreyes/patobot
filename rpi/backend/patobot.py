abddef21e9099da6631c528eab052a1cfcff7137
Dyego Cantu <c4ntuz@gmail.com>
2015-07-02 00:09:19 -0300

    Valores como string para enviar para o Arduino

--- rpi/backend/patobot.py
+++ rpi/backend/patobot.py
@@ -14,26 +14,26 @@ except:
 app = Flask(__name__)
 
 direcoes_andar = {
-    'frente': 1,
-    'tras': 2,
+    'frente': '1',
+    'tras': '2',
 }
 
 direcoes_girar = {
-    'direita': 3,
-    'esquerda': 4,
+    'direita': '3',
+    'esquerda': '4',
 }
 
 
 @app.route('/andar/<direcao>')
 def andar(direcao):
-    com.write(str(direcoes_andar[direcao]))
-    return 'PatoBot andou para %s' % direcao
+    com.write(direcoes_andar[direcao])
+    return 'PatoBot andou para %s!' % direcao
 
 
 @app.route('/girar/<direcao>')
 def girar(direcao):
-    com.write(str(direcoes_girar[direcao]))
-    return 'PatoBot girou para %s' % direcao
+    com.write(direcoes_girar[direcao])
+    return 'PatoBot girou para %s!' % direcao
 
 
 @app.route('/')

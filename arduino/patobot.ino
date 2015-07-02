d04af7edb89c5ece4b2c994f09a1c8893cef8ac4
Dyego Cantu <c4ntuz@gmail.com>
2015-07-01 21:08:49 -0300

    Criado diretórios para cada subprojeto

---
+++ arduino/patobot.ino
@@ -0,0 +1,21 @@
+int LED = 13;
+int inByte = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0) {
+     inByte = Serial.read() - '0';
+     int i = 0;
+     while(i < inByte) {
+       digitalWrite(LED, HIGH);
+       delay(1000);
+       digitalWrite(LED, LOW);
+       delay(1000);
+       i++;
+     }
+  }
+}

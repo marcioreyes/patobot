f6d24c285462f360a481131643041dd0a9c84d21
Guilherme Berghauser <tortoyoyo@gmail.com>
2015-08-10 17:45:27 -0300

    ajustado o frontend e adicionado suporte ao localstorage

--- rpi/frontend/js/app.js
+++ rpi/frontend/js/app.js
@@ -1,45 +1,108 @@
-  var ip = 'http://192.168.1.2:5000';
+/*-----------------------
+----------CONSTRUTOR-----
+-------------------------*/
+var localStorage = verificaStorage();
+var padrao = '192.168.1.2:5000';
 
-  var values = {
-    url: '',
-    rest: 'GET',
-    async: false
-  };
+function verificaStorage() {
+  try {
+    return 'localStorage' in window && window['localStorage'] !== null;
+  } catch (e) {
+    return false;
+  }
+}
 
-  var frente = function() {
-    values.url = ip + '/andar/frente';
-    ajax(values, function(resp) {
-      console.log(resp.response);
-    });
-  };
+function mudarIp(){
+  var ipMudanca = prompt("Insira um ip com a porta. Ex: 192.168.1.2:5000", ip);
 
-  var tras = function() {
-    values.url = ip + '/andar/tras';
-    ajax(values, function(resp) {
-      console.log(resp.response);
-    });
-  };
+  if (ipMudanca != null) {
+    ip = ipMudanca;
+    localStorage['ip'] = ip;
+  }
+}
 
-  var direita = function() {
-    values.url = ip + '/girar/direita';
-    ajax(values, function(resp) {
-      console.log(resp.response);
-    });
-  };
+function ajax(values, callback) {
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener('load', callback(oReq));
+  oReq.open(values.rest, values.url, values.async);
+  oReq.send();
+}
 
-  var esquerda = function() {
-    values.url = ip + '/girar/esquerda';
-    ajax(values, function(resp) {
-      console.log(resp.response);
-    });
-  };
+/*-----------------------
+------FIM CONSTRUTOR-----
+-------------------------*/
 
+/*-----------------------
+-------- INIT -----------
+-------------------------*/
 
-  var ajax = function(values, callback) {
+var ip = '';
 
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener('load', callback(oReq));
-    oReq.open(values.rest, values.url, values.async);
-    oReq.send();
+if(localStorage){
+  if(localStorage['ip']){
+    ip = localStorage['ip'];
+  } else {
+    ip = padrao;
+    mudarIp();
+  }
+} else {
+  ip = padrao;
+  mudarIp();
+}
 
-  };
+var values = {
+  url: '',
+  rest: 'GET',
+  async: false
+};
+
+/*-----------------------
+--------FIM INIT --------
+-------------------------*/
+
+/*-----------------------
+--------MOVIMENTOS-------
+-------------------------*/
+
+var frente = function() {
+  values.url = ip + '/andar/frente';
+  ajax(values, function(resp) {
+    console.log(resp.response);
+  });
+};
+
+var tras = function() {
+  values.url = ip + '/andar/tras';
+  ajax(values, function(resp) {
+    console.log(resp.response);
+  });
+};
+
+var direita = function() {
+  values.url = "http://"+ip + '/girar/direita';
+  ajax(values, function(resp) {
+    console.log(resp.response);
+  });
+};
+
+var esquerda = function() {
+  values.url = ip + '/girar/esquerda';
+  ajax(values, function(resp) {
+    console.log(resp.response);
+  });
+};
+
+/*-----------------------
+--------FIM MOVIMENTOS---
+-------------------------*/
+
+/*-----------------------
+--------OUTRAS ACOES-------
+-------------------------*/
+
+var limparDados = function(){
+  var resposta = confirm("Deseja limpar informações de IP?");
+  if (resposta) {
+    localStorage.clear();
+  }
+}
